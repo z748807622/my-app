daaa270c7d82183eceda421488961b9d1c64abdb
ZJY <748807622@qq.com>
2019-03-12 16:11:02 +0800

    增加聊天室，心跳机制

--- src/components/OnlineChat.vue
+++ src/components/OnlineChat.vue
@@ -1,15 +1,26 @@
 <template>
     <div class="left_div">
-        <div class="display_msg"></div>
+        <div class="display_msg">
+            <div style="height: 30px;">
+                <span style="float: left">nick:{{myNickName}}</span>
+                <span style="float: right">在线人数:{{onlineNum}}</span>
+            </div>
+            <span v-for="todo in lists">
+                <el-card shadow="hover">
+                    {{todo.user}}:{{todo.msg}}
+                </el-card>
+            </span>
+        </div>
         <div class="sendMsg">
             <el-input
                     class="msg_input"
                     type="textarea"
                     :rows="2"
                     placeholder="请输入内容"
+                    v-on:keyup.enter.native="sendMsg"
                     v-model="textarea">
             </el-input>
-            <el-button type="primary" class="iconfont" icon="icon-send">发送</el-button>
+            <el-input v-if="!isLogin" v-model="nickName" placeholder="输入nickName" v-on:keyup.enter.native.stop="login"></el-input>
         </div>
     </div>
 </template>
@@ -20,16 +31,98 @@
         data(){
             return{
                 textarea: '',
-                msgUrl: ''
+                nickName:'',
+                isLogin:false,
+                websocket:null,
+                myNickName:'',
+                onlineNum:0,
+                pingTimer:null,
+                lists:[]
             }
         },
+        created(){
+            this.initWebSocket()
+        },
         mounted() {
-            /*console.log("lll",$('.left_div').height(),$('.sendMsg').height(),$(".display_msg").height())
-            $(".display_msg").css('height',$('.left_div').height() - $('.sendMsg').height() + 'px')
-            $(window).resize(function () {
-                console.log("lll",$('.left_div').height(),$('.sendMsg').height(),$(".display_msg").height())
-                $(".display_msg").css('height',$('.left_div').height() - $('.sendMsg').height() + 'px')
-            })*/
+
+        },
+        destroyed(){
+            this.websocket.close();
+        },
+        methods:{
+            initWebSocket(){
+                const wsuri = "ws://localhost:9999/ws"
+                this.websocket = new WebSocket(wsuri);
+                this.websocket.onopen = this.wsOpen();
+                this.websocket.onerror = this.wsError;
+                this.websocket.onmessage = this.wsMessage;
+                this.websocket.onClose = function () {
+                    //console.log("聊天室关闭");
+                    this.$message({message:'聊天室关闭',type:'warn'});
+                }
+            },
+            wsOpen(){
+                this.$message({message:'聊天室连接成功',type:'success'});
+                //console.log("聊天室连接成功");
+            },
+            wsError(e){
+                console.log("聊天室错误:",e);
+                this.$message.error('聊天室错误');
+                clearInterval(this.pingTimer);//清除心跳
+                this.isLogin = false;
+            },
+            wsMessage(e){
+                console.log("聊天室接收消息：",e.data)
+                var data = JSON.parse(e.data);
+
+                if(data.extend && data.extend.code == 20001){//接收在线人数信息
+                    this.onlineNum = data.extend.mess;
+                }
+
+                if(data.extend && data.extend.code == 20002 && data.extend.mess){//认证登陆
+                    this.myNickName = this.nickName;
+                    this.$message({message:'nick登陆成功',type:'success'});
+                    this.isLogin = true;
+
+                    this.pingTimer = setInterval(()=>{//浏览器链接心跳
+                        this.ping();
+                    },10000);
+
+                }else if (data.extend && data.extend.code == 20002 && !data.extend.mess) {
+                    this.$message.error('nick登陆失败');
+                }
+
+                if (data.body){
+                    var dd = {
+                        msg:data.body,
+                        user:data.extend.nick
+                    }
+                    this.lists.push(dd);
+                }
+            },
+            login(){
+                var loginMse = {
+                    nick:this.nickName,
+                    code:10000
+                }
+                console.log("nickName",loginMse)
+                this.websocket.send(JSON.stringify(loginMse));
+            },
+            sendMsg(){
+                console.log("发送消息:",this.textarea)
+                var sendMsg = {
+                    code:10086,
+                    mess:this.textarea
+                }
+                this.websocket.send(JSON.stringify(sendMsg));
+                this.textarea = "";
+            },
+            ping(){//发送ping
+                var pingMsg = {
+                    code:10015
+                }
+                this.websocket.send(JSON.stringify(pingMsg));
+            }
         }
     }
 
@@ -46,7 +139,8 @@
 
     .display_msg{
         height: 85%;
-        background: #55a532;
+        //background: #55a532;
+        padding: 5px 10px;
     }
 
     .sendMsg .msg_input{
